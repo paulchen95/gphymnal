3372baa1a215d4e2e2774438c34652b21e7b6c09
jp <1630377+jaypark@users.noreply.github.com>
2022-02-07 01:20:01 -0800

    Implement Search feature

--- GP Hymnal/ContentView.swift
+++ GP Hymnal/ContentView.swift
@@ -9,16 +9,64 @@ import SwiftUI
 import Foundation
 
 struct ContentView: View {
+    @State private var searchText = ""
+    @State private var showCancelButton: Bool = false
+    @State private var showHymnList: Bool = true
+    
     var body: some View {
         NavigationView {
-            List {
-                let hymnList = HymnList()
-                ForEach(hymnList.hymns, content: {
-                    hymn in
-                    NavigationLink(hymn.name, destination:
-                                    DetailsView(hymn: hymn))
-                })
-            }
+            VStack {
+                HStack {
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                        TextField("Search...", text: $searchText, onEditingChanged: { isEditing in
+                            self.showCancelButton = true
+                            self.showHymnList = false
+                        }, onCommit: {
+                            print("onCommit")
+                        }).foregroundColor(.primary)
+                        
+                        Button(action: {
+                            self.searchText = ""
+                            self.showHymnList = true
+                        }) {
+                            Image(systemName: "xmark.circle.fill").opacity(searchText == "" ? 0 : 1)
+                        }
+                    }
+                    .padding(EdgeInsets(top: 8, leading: 6, bottom: 8, trailing: 6))
+                    .foregroundColor(.secondary)
+                    .background(Color(.secondarySystemBackground))
+                    .cornerRadius(10.0)
+                    
+                    if showCancelButton  {
+                        Button("Cancel") {
+                            UIApplication.shared.endEditing(true) // this must be placed before the other commands here
+                            self.searchText = ""
+                            self.showCancelButton = false
+                            self.showHymnList = true
+                        }.foregroundColor(Color(.systemBlue))
+                    }
+                }.padding(.horizontal)
+                
+                Divider()
+                List {
+                    let hymnList = HymnList()
+                    if showHymnList {
+                        ForEach(hymnList.hymns, content: {
+                            hymn in
+                            NavigationLink(hymn.name, destination: DetailsView(hymn: hymn))
+                        })
+                    } else {
+                        // Filtered list of names
+                        ForEach(hymnList.hymns.filter {
+                            $0.name.lowercased().contains(searchText.lowercased())
+                        }, content: {
+                            hymn in
+                            NavigationLink(hymn.name, destination: DetailsView(hymn: hymn))
+                        })
+                    }
+                }
+            }.navigationBarHidden(true)
         }
     }
 }
@@ -26,5 +74,14 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+extension UIApplication {
+    func endEditing(_ force: Bool) {
+        self.windows
+            .filter{$0.isKeyWindow}
+            .first?
+            .endEditing(force)
     }
 }
