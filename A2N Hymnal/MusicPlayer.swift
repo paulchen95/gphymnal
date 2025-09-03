3fc3da9a3cc23fcfd7d0f5da34120ed66dbc1447
Paul Chen <88908993+paulchen95@users.noreply.github.com>
2025-09-03 09:21:05 -0500

    get rid of more force-unwrapping + fix stop btn + fix pause btn
    - take out of force-unwrapping for
      - DetailsView
      - MuiscPlayer
    - fix stop btn showing up when there is no mp3 to play
    - After MP3 finishes → Button automatically changes from "Pause" back to "Play"

--- A2N Hymnal/MusicPlayer.swift
+++ A2N Hymnal/MusicPlayer.swift
@@ -12,38 +12,40 @@ enum PlayerState {
     case Playing, Stopped, Paused
 }
 
-class Mp3Player {
+class Mp3Player: NSObject, ObservableObject, AVAudioPlayerDelegate {
     var mp3File: URL?
     var player: AVAudioPlayer?
-    var state: PlayerState = PlayerState.Stopped
+    @Published var state: PlayerState = PlayerState.Stopped
 
     init (name: String) {
+        super.init()
         mp3File = Bundle.main.url(forResource: name, withExtension: "mp3", subdirectory: "Music")
         if let mp3File = mp3File {
             player = try? AVAudioPlayer(contentsOf: mp3File)
+            player?.delegate = self
         }
     }
     
     func play() -> PlayerState {
-        if player!.prepareToPlay() {
-            player!.play()
+        if let player = player, player.prepareToPlay() {
+            player.play()
             state = PlayerState.Playing
         }
         return state
     }
     
     func paused() -> PlayerState {
-        if isAvailable() && player != nil {
-            player!.pause()
+        if isAvailable(), let player = player {
+            player.pause()
             state = PlayerState.Paused
         }
         return state
     }
 
     func stop() -> PlayerState {
-        if isAvailable() && player != nil {
-            player!.stop()
-            player!.currentTime = 0.0
+        if isAvailable(), let player = player {
+            player.stop()
+            player.currentTime = 0.0
             state = PlayerState.Stopped
         }
         return state
@@ -51,5 +53,10 @@ class Mp3Player {
 
     func isAvailable() -> Bool {
         return mp3File != nil
+    }
+    
+    // MARK: - AVAudioPlayerDelegate
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        state = PlayerState.Stopped
     }
 }
